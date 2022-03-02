6f5e209c79ea91130505c1ef67a2bae401075f07
Firmansya <filayudafirmansya.com>
2022-03-02 11:56:54 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
+import React, {Component} from 'react';
+import {BrowserRouter,Route, Link} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function Home() { 
+  return <h2>ini halaman home</h2>
+}
+function listView() { 
+  return <h2>ini halaman Users</h2>
+}
+
+
+
+class App extends Component { 
+  render() { 
+    return (
+      <BrowserRouter>
+        <div>
+          <nav>
+            <li> <Link to='/'>Home</Link> </li>
+            <li> <Link to='/users'>User</Link> </li>
+          </nav>
+
+          <main>
+            <Route path='/' exact component={Home} />
+            <Route path='/users' exact component={listView} />
+          </main>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
